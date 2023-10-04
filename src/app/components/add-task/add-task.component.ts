9ad16e5568637e0dbb7ac0aae44015034a6ce3f0
stefanpython <stefanhtml@yahoo.com>
2023-10-04 17:45:59 +0300

    Add toggle for adding task form

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,6 +1,7 @@
 import { Component, Output, EventEmitter } from "@angular/core";
-import { faL } from "@fortawesome/free-solid-svg-icons";
 import { Task } from "src/app/Task";
+import { UiService } from "src/app/services/ui.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-add-task",
@@ -12,6 +13,14 @@ export class AddTaskComponent {
   text!: string;
   day!: string;
   reminder: boolean = false;
+  showAddTask!: boolean;
+  subscription!: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   onSubmit() {
     if (!this.text) {
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from "@angular/core";
+import { UiService } from "src/app/services/ui.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-header",
@@ -7,8 +9,16 @@ import { Component } from "@angular/core";
 })
 export class HeaderComponent {
   title: string = "Tasks Tracker";
+  showAddTask!: boolean;
+  subscription!: Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   toggleAddTask() {
-    console.log("button");
+    this.uiService.toggleAddTask();
   }
 }
